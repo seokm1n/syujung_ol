176e90983dd6b58f8642ebaa7ee74e7b46a90847
suckm <suckm@DESKTOP-DUMRNUO>
2025-02-19 15:34:51 +0900

    564

--- jung_ol/src/q564/Main.java
+++ jung_ol/src/q564/Main.java
@@ -1,6 +1,6 @@
 package q564;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
@@ -9,15 +9,17 @@ public class Main {
 		for (int i = 0; i < 26; i++) {
 			arr[i] = 0;
 		}
-		
+
 		while (true) {
 			char ch = sc.next().charAt(0);
-			if (ch < 'A' || ch > 'Z')
+			if (ch <= 'A' && ch >= 'Z') {
+				arr[(int) ch - 65]++;
+			} else {
 				break;
-			arr[(int) ch - 65]++;
+			}
 		}
 		sc.close();
-		
+
 		for (int i = 0; i < 26; i++) {
 			if (arr[i] != 0) {
 				System.out.print((char) (int) (i + 65) + " : ");
